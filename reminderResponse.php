48e7d375d7ddde92297d566f1199704fd3ed91ff
Ahmad Hassan <ahmadhassanyaseen@gmail.com>
2025-10-03 17:49:48 +0500

    new changes

--- reminderResponse.php
+++ reminderResponse.php
@@ -1,9 +1,13 @@
 <?php
 include 'config/config.php';
+// print_r($_GET);
 if(isset($_GET['id']) && $_GET['id'] != ''){
     $data['id'] = $_GET['id'];
     $data['action'] = $_GET['action'];
+
+
     $response = updateReminderForShipper($data);
+    // print_r($response);
 }
    
 ?>
